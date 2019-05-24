9a17f7b93f279e9d549714302a9a47f38dad3a75
khyreerusydi <nicoraventbh@gmail.com>
2019-05-24 11:40:31 +0800

    switched to boilerplate code, base extension works

---
+++ focusnow-chrome-extension/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        date: new Date()
+    };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <h1>It's time to take charge of your life.</h1>
+        <h2>{this.state.date.toLocaleTimeString('en-SG')}.</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
