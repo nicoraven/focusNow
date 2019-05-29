52ecc3d1f1c46d32acc3fb8e05b7d0261076cdd1
khyreerusydi <nicoraventbh@gmail.com>
2019-05-29 23:58:14 +0800

    created settings menu

--- focusnow-chrome-extension/src/settings.js
+++ focusnow-chrome-extension/src/settings.js
@@ -8,16 +8,43 @@ class Settings extends Component {
         super(props);
     }
 
+    toggleMenu = (event) => {
+        let menu = document.querySelector(".settings-content");
+        if (menu.style.display === "none") {
+            menu.style.display = "";
+        } else {
+            menu.style.display = "none";
+        }
+    }
+
+    toggleDisplay = (event) => {
+        let menu = event.target.nextSibling;
+        if (menu.style.display === "none") {
+            menu.style.display = "";
+        } else {
+            menu.style.display = "none";
+        };
+    }
+
     render() {
         console.log("rendering settings");
 
         return (
             <div className="settings">
-                <img className="icon" src={gear} alt="settings" />
-                <div class="settings-content">
-                    <div className="background-settings">
+                <img
+                    className="icon"
+                    src={gear}
+                    alt="settings"
+                    onClick={this.toggleMenu}
+                />
+                <div class="settings-content" style={{display: "none"}} >
+                    <div
+                        className="background-settings"
+                    >
                         <p>Choose a background colour</p>
-                        <ColourPicker />
+                        <div className="setting-options" style={{display: ""}}>
+                            <ColourPicker />
+                        </div>
                     </div>
                 </div>
             </div>
